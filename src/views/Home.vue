4c4d8bf921fd0b9486796ebaf5c884ca97a7e20a
tedet <tedteah@gmail.com>
2023-04-25 09:52:32 -0400

    fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,11 @@
   </div>
   <div class="caption" >
   <h1 class="title">Island Craft Shop</h1>
-  <p class="lead">An artisan cooperative.</p>
+  <p class="lead" >An artisan cooperative</p>
   <hr class="my-4">
-  <p>The Island Craft Shop at 329 US Route 2 in South Hero Village is the oldest artisan cooperative in Grand Isle County, Vermont</p>
+  <p>The Island Craft Shop (ICS) at 329 US Route 2 in South Hero Village is the oldest artisan cooperative in the Lake Champlain Region</p>
   <p class="lead">
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  <a class="btn btn-primary btn-lg" href="#/about" role="button">Learn more</a>
   </p>
   </div>
   <div class="notes">NOTES
