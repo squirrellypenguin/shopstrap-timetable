0a31fb5cc5157e05a4da1948c628b0671e2bc9e9
tedet <tedteah@gmail.com>
2023-04-25 12:57:58 -0400

    added picture to front page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
- <div class="container">
+  <div class="container">
   <Media />
   </div>
   <div class="caption" >
@@ -8,9 +8,17 @@
   <p class="lead" >An artisan cooperative</p>
   <hr class="my-4">
   <p>Island Craft Shop (ICS) located at 329 US Route 2 in South Hero Village is the oldest artisan cooperative in the Lake Champlain Region</p>
+  
+  <div class="row" >
+  <div class="column conditional">
+  <p>dsfsdfdsfdsfds</p>
+  </div>
+  <div class="column">
   <p class="lead">
   <a class="btn btn-primary btn-lg" href="#/about" role="button" style="font-weight: bold; letter-spacing: .1rem;">Learn more</a>
   </p>
+  </div>
+  </div>
   </div>
   <div class="notes">NOTES
   </div>
