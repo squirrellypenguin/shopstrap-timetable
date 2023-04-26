334f92bf932b7923f0d537bd280931fad751c61d
tedet <tedteah@gmail.com>
2023-04-26 08:43:37 -0400

    further styling on about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="about">
 
-    <h1>History of the Island Craft Shop</h1>
+    <h1 class="small">Island Craft Shop's Story</h1>
 
-    <h2>From modest beginnings</h2>
+    <h2 class="smaller">From modest beginnings</h2>
     <div class="sign" >
-          <p style="padding-left: 20px; padding-right: 25px; text-align: justify;">It was founded in 1991 by a group of talented Champlain Island crafters including Sue Alenick, Mary Jane Healy, and Ethel Schuele. This is a member-owned and operated business focused on the arts and not profits. Over the years, the shop has moved several times and members have come and gone, but the ICS continues thrive, offering the best and most innovative Vermont products.</p>
+          <p style="padding-left: 20px; padding-right: 25px; text-align: justify;">It was founded in 1991 by a group of talented Champlain Island crafters including Sue Alenick, Mary Jane Healy, and Ethel Schuele. 
+            <br>
+            <br>
+            This is a member-owned and operated business focused on the arts and not profits. Over the years, the shop has moved several times and members have come and gone, but the ICS continues thrive, offering the best and most innovative Vermont products.</p>
     </div>
+    <br>
     <h2> Our forever home</h2>
-    <p> The shop has grown into a must-see destination for Islanders and visitors alike.  Over twenty members now exhibit a wide-ranging array of unique crafts. You will find a fine assortment of items: fiber goods; pottery; fused & stained glass art; a mutlitude of jewelry; turned wooded items; watercolor & acrylic paintings; candles; garden ornaments; and many other made-in-Vermont goods. You'll find gift ideas for showers, weddings, birthdays, holidays and festivies of all make.</p>
-
+    <div class="sign">
+      <p style="padding-left: 20px; padding-right: 25px; text-align: justify;">The shop has grown into a must-see destination for Islanders and visitors alike.  Over twenty members now exhibit a wide-ranging array of unique crafts. 
+        <br>
+        <br>
+        You will find a fine assortment of items: fiber goods; pottery; fused & stained glass art; a mutlitude of jewelry; turned wooded items; watercolor & acrylic paintings; candles; garden ornaments; and many other made-in-Vermont goods. You'll find gift ideas for showers, weddings, birthdays, holidays and festivies of all make.</p>
+    </div>
    </div>
 </template>
